7bc606520356cb86bda0ab2fa18edcfbbe1603cb
huqa <pikkuhukka@gmail.com>
2015-01-23 13:21:18 +0200

    Huge update: made a nicer UI and updated the game logic.

--- js/client.js
+++ js/client.js
@@ -2,6 +2,9 @@
 	var is_my_turn = false;
 	var is_intermission = false;
 	var the_word = "";
+	var word_data = {};
+	var word_length = 0;
+	var drawer = "";
 
 	var painter = null;
 
@@ -12,10 +15,16 @@
 		name: 'Welcome to draw and guess! Whats your name?'
 	};
 
+	var _PHASE = {
+		not_started: 'Game not started',
+		intermission: 'Intermission',
+		cooldown: 'Cool down',
+		dg: 'Draw and Guess!'
+	};
+
 	// on connection to server, ask for user's name with an anonymous callback
 	socket.on('connect', function(){
 		// call the server-side function 'adduser' and send one parameter (value of prompt)
-		// TODO clear UI
 		var name = null;
 		while(name === null || name === "") { name = prompt(_MSGS.name); }
 		socket.emit('add_user', name);
@@ -28,34 +37,34 @@
 		say(username, data);
 	});
 
-	// listener, whenever the server emits 'update_users', this updates the username list
-	socket.on('update_users', function(data) {
+	// listener, whenever the server emits 'update_users', this updates the username list and scores
+	socket.on('update_users', function(usernames, scores) {
 		$('#users').empty();
-		$.each(data, function(key, value) {
-			$('#users').append('<div>' + key + '</div>');
-		});
+		for(var user in usernames) {
+			$('#users').append('<div class="user"><span class="username">' + usernames[user] + '</span><span class="score">'+scores[user]+'</span></div>');
+		}
 	});
 	
 	//on img update
 	socket.on('update_img', function() {
 		if (is_my_turn === false && painter !== null) {
-	    	var src = 'out.png?' + new Date().getTime(); // Set source path 
-	    	//console.log(src);
-	    	//img.onload = function(){  
-	      	// execute drawImage statements here  This is essential as it waits till image is loaded before drawing it.
-	 	painter.setImg(src);
-	    	//};
+	    		var src = 'out.png?' + new Date().getTime(); // Set source path
+	      		// execute drawImage statements here  This is essential as it waits till image is loaded before drawing it.
+	 		painter.setImg(src);
 		}
 	});
+
 	// sets the word to draw for the drawer
 	socket.on('word_to_draw', function(word) {
 		the_word = word;
 	});
+
 	// counter, game_start, game_stop, next_drawer, intermission_counter_s/s, drawer_counter_s/s
 	socket.on('game_start', function() {
 		handleDrawingBoard();
 		handleChat();
 	});
+
 	socket.on('game_stop', function() {
 		handleDrawingBoard();
 		handleChat();
@@ -63,14 +72,65 @@
 		$('#iwannaplay').prop('value', 'iwannaplay');
 		$('#iwannaplay').html("I wanna play");
 		painter.resetBackground();
+		$('#game-phase').html(_PHASE.not_started);
+	});
 
-	});	
 	socket.on('is_drawing', function(what) {
 		is_my_turn = what;
 	});
 
+	socket.on('word_data', function(data) {
+		word_data = data;
+		word_length = data.word_length;
+		if(is_my_turn === false) {
+			$("#word").html("("+data.type+")");
+		}
+	});
+
+	/*socket.on('word_type', function(type) {
+		if(is_my_turn === false) {
+			$("#word").html("("+type+")");
+		}	
+	});
+
+	socket.on('word_length', function(length) {
+		word_length = length;
+	});*/
+
+	socket.on('reveal_first_letter', function(letter) {
+		if(is_my_turn === false) {
+			var rev_word = letter;
+			for(var k = 1; k < word_length; k++) {
+				if(word_data.has_whitespace === true) {
+					if(k == word_data.whitespace_index) {	
+						rev_word += " ";
+					} else {
+						rev_word += "_";
+					}
+				} else {
+					rev_word += "_";
+				}
+			}
+			var word_type = $("#word").html();
+			$("#word").html(word_type +" "+ rev_word);
+		}
+	});
+
+	socket.on('reveal_last_letter', function(letter) {
+		if(is_my_turn === false) {
+			var full_word = $("#word").html();
+			full_word = full_word.substr(0, full_word.length-1) + letter;
+			$("#word").html(full_word);
+		}
+	});
+
+	socket.on('drawer', function(name) {
+		$('#drawer-name').html(name);
+	});
+
 	socket.on('intermission_counter_start', function() {
 		is_intermission = true;
+		$('#game-phase').html(_PHASE.intermission);
 		painter.resetBackground();
 		handleDrawingBoard();
 		handleChat();
@@ -80,13 +140,14 @@
 		server_say(message);
 		if(is_my_turn === true) {
 			//var msg = '<div class="message"><b>!!!! YOUR WORD is </b> ' + the_word + '<br></div>';
-		        var msg = _MSGS.yourWord + the_word + '>>>';
+		        var msg = _MSGS.yourWord + the_word + ' >>>';
 			msg = bold_msg(msg); 
 			server_say(msg);
 			$('#word').html(the_word);
 		} else {
 			the_word = "";
 			$('#word').html("");
+			word_data = {};
 		}
 	});
 
@@ -98,7 +159,12 @@
 	socket.on('drawer_counter_start', function() {
 		//var msg = '<div class="message"><b>ROUND START::</b><br></div>';
 		var msg = bold_msg(_MSGS.roundStart);
+		$('#game-phase').html(_PHASE.dg);
 		server_say(msg);
+	});
+
+	socket.on('victory_counter_start', function() {
+		$('#game-phase').html(_PHASE.cooldown);
 	});
 	
 	socket.on('counter', function(count) {
@@ -122,7 +188,6 @@
 	}
 
 	function say(username, data) {
-		//var out = '<div class="message"><b>'+ username +'</b> ' + data + '<br></div>';
 		var out = say_msg(username, data);
 		$('#conversation').append(out);
 		$('#conversation').scrollTop($('#conversation')[0].scrollHeight);		
@@ -145,8 +210,9 @@
 
 	/** Decorates a message with say-markup */
 	function say_msg(username, data) {
-		return '<div class="message"><b>'+ username +'</b> ' + data + '<br></div>';
+		return '<div class="message">&lt;'+ username +'&gt; ' + data + '<br></div>';
 	}
+
 	/** Decorates a message with default markup */
 	function default_msg(data) {
 		return '<div class="message">'+data+'<br></div>';
@@ -158,7 +224,6 @@
 
 	// on page load
 	$(function(){
-		
 		// when the client clicks SEND
 		$('#datasend').click( function() {
 			var message = $('#data').val();
@@ -171,10 +236,12 @@
 				socket.emit('wanna_play', true);
 				$(this).prop('value', 'dontwanna');
 				$(this).html("I don't wanna play");
+				$(this).attr('class', 'btn btn-warning');
 			} else {
 				socket.emit('wanna_play', false);
 				$(this).prop('value', 'iwannaplay');
 				$(this).html("I wanna play");
+				$(this).attr('class', 'btn btn-success');
 			}
 			handleDrawingBoard();
 			handleChat();
@@ -207,6 +274,5 @@
 				socket.emit('img_send', dataUrl);
 			}
 		});
-
 	});
 
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 /*
- * Piirrä & arvaa - nodejs server
+ * draw-and-guess node server and game logic
+ *
+ * TODO: send note of white space in word to clients
+ *
  * @author Ville Riikonen // huqa - pikkuhukka@gmail.com
- *
- * TODO&NOTES: Database too slow to fetch word. Need to set a timeout? or something 
- *
- *
  */
 
 
@@ -29,12 +28,10 @@ if(!db_file_exists) {
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(db_file);
 
-// Create empty db
 db.serialize(function() {
 	if(!db_file_exists) {
-		db.run('CREATE TABLE words(id int PRIMARY KEY, word TEXT)');
-		db.run('INSERT INTO words(word) VALUES("bulle")');
-		// Possibly create scores
+		db.run('CREATE TABLE words(id int PRIMARY KEY, word TEXT, type TEXT)');
+		db.run('INSERT INTO words(word, type) VALUES("bulle", "subs.")');		
 	}
 });
 db.close();
@@ -45,10 +42,10 @@ var usernames = {};
 // All users wanting to play
 var players = {};
 // scorelist
-var score = {};
+var scores = {};
 
 // Simple queue implementation
-// TODO a real queue with sorting --
+// TODO a real queue
 function queue(){var a,b,c,d;return d=function(e){e!=c?(b=b?b.n={v:e}:a={v:e},e=d):(e=a?a.v:c,a=a==b?b=c:a.n);return e}}
 var player_queue = queue();
 // Adds player to the queue
@@ -73,29 +70,27 @@ var counter = 0;
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
-//route for jquery -- i guess we need to do this for all external js-files
-//app.get('/jquery.js', function(req, res) {
-//	res.sendfile(__dirname + '/js/lib/jquery.js');
-//});
+
 app.use(express.static(__dirname + '/js'));
 app.use(express.static(__dirname + '/js/lib'));
 app.use(express.static(__dirname + '/img'));
 app.use(express.static(__dirname + '/css'));
 
 var game = new function() {
-	this.max_time = 180;
+	this.max_time = 120;
 	this.intermission_time = 15;
 	this.victory_timer = 8;
 	// The game is running (includes intermissions etc.)
 	this.is_running = false;
 	// Is the game in intermission
 	this.is_intermission = false;
+	this.is_cooldown = false;
 	// Is the drawer currently drawing
 	this.player_is_drawing = false;
 	this.drawer = "";
 	this.timer;
 	this.the_word = "";
-	this.the_word_id = "";
+	this.the_word_data = {};
 	this.start_timer = function() {
 		this.player_is_drawing = true;
 		io.sockets.emit('drawer_counter_start');
@@ -105,15 +100,14 @@ var game = new function() {
 	this.stop_timer = function() {
 		clearInterval(this.timer);
 		counter = 0;
-		//update the client side as well
 	};
 	this.start_intermission = function() {
-		// here we should broadcast the word to be guessed for the drawer - oh ja se pitää arpoo
 		var id = find_socket_by_name(this.drawer);
 		//this.fetch_word();
-		//sqlite fetch here
 		io.sockets.sockets[id].emit('word_to_draw', this.the_word);
 		io.sockets.emit('intermission_counter_start');
+		io.sockets.emit('drawer', this.drawer);
+		io.sockets.emit('word_data', this.the_word_data);
 		this.is_intermission = true;
 		this.player_is_drawing = false;
 		counter = this.intermission_time;
@@ -124,13 +118,22 @@ var game = new function() {
 		if(counter <= 0) {
 			game.stop_timer();
 			io.sockets.emit('drawer_counter_stop');
-			var id = find_socket_by_name(this.drawer);
-			io.sockets.sockets[id].is_drawer = false;	
+			var id = find_socket_by_name(game.drawer);
+			io.sockets.sockets[id].is_drawer = false;
 			io.sockets.sockets[id].emit('is_drawing', false);
-			// No one guessed the word so... next drawer?
-			game.next_drawer();
+			// No one guessed the word so show the word and next drawer?
+			io.sockets.emit('update_chat', 'SERVER', 'No one guessed the word: ' + game.the_word);
+                	io.sockets.emit('victory_counter_start');
+		        counter = game.victory_timer;
+			game.timer = setInterval(game.victory_decrement, 1000);
+
 		} else {
 			io.sockets.emit('counter', counter);
+			if(counter == 90) {
+				io.sockets.emit('reveal_first_letter', game.the_word[0]);
+			} else if(counter == 30) {
+				io.sockets.emit('reveal_last_letter', game.the_word[game.the_word.length-1]);
+			}
 		}
 	};
 	this.intermission_decrement = function(self) {
@@ -138,7 +141,7 @@ var game = new function() {
 		if(counter <= 0) {
 			game.stop_timer();
 			io.sockets.emit('intermission_counter_stop');
-			this.is_intermission = false;
+			game.is_intermission = false;
 			game.start_timer();
 		} else {
 			io.sockets.emit('counter', counter);
@@ -149,6 +152,7 @@ var game = new function() {
 		if(counter <= 0) {
 			game.stop_timer();
 			io.sockets.emit('victory_counter_stop');
+			game.is_cooldown = false;
 			game.next_drawer();
 		} else {
 			io.sockets.emit('counter', counter);
@@ -159,8 +163,6 @@ var game = new function() {
 		this.drawer = player_queue();
 		// here we should change the socket session variable .is_drawer
 		var id = find_socket_by_name(this.drawer);
-		//console.log(id);
-		//console.log(io.sockets.sockets);
 		io.sockets.sockets[id].is_drawer = true;
 		io.sockets.sockets[id].emit('is_drawing', true);
 		io.sockets.sockets[id].emit('update_chat', 'SERVER', "It's your turn to draw.");
@@ -170,25 +172,36 @@ var game = new function() {
 	};
 	this.stop = function() {
 		var id = find_socket_by_name(this.drawer);
-		io.sockets.sockets[id].is_drawer = false;
-		io.sockets.sockets[id].emit('is_drawing', false);
+		if(typeof id !== "undefined") {
+			io.sockets.sockets[id].is_drawer = false;
+			io.sockets.sockets[id].emit('is_drawing', false);
+		}
 		this.is_running = false;
 		this.is_intermission = false;
 		this.player_is_drawing = false;
 		this.drawer = "";
 		this.the_word = "";
+		this.the_word_type = "";
 		this.stop_timer();
 		player_queue = queue();
+		rebuildqueue();
 		io.sockets.emit('game_stop');
 	};
-	this.guessed_right = function() {
+	this.guessed_right = function(guesser) {
+		var score_count = counter;
 		this.player_is_drawing = false;
 		this.the_word = "";
+		this.the_word_type = "";
 		this.stop_timer();
 		var id = find_socket_by_name(this.drawer);
+		// Drawer gets 5 percent bonus
+		scores[this.drawer] += score_count + Math.floor(score_count * 0.05);
+		scores[guesser] += score_count;
 		io.sockets.sockets[id].is_drawer = false;
 		io.sockets.sockets[id].emit('is_drawing', false);
+		io.sockets.emit('update_users', usernames, scores);
 		io.sockets.emit('victory_counter_start');
+		this.is_cooldown = true;
 		counter = this.victory_timer;
 		this.timer = setInterval(this.victory_decrement, 1000);
 		// intermission, new word, next drawer
@@ -211,7 +224,7 @@ var game = new function() {
 		this.fetch_word_and_start_intermission();
 	};
 	this.is_drawer = function(player) {
-		if (this.drawer === player) {
+		if (game.drawer === player) {
 			return true;
 		} else {
 			return false;
@@ -223,6 +236,14 @@ var game = new function() {
 			db.get("SELECT * FROM words ORDER BY RANDOM() LIMIT 1", function(error, row) {
 				if(typeof row !== "undefined") { 
 					game.the_word = row.word;
+					game.the_word_data.type = row.type;
+					game.the_word_data.word_length = row.word.length;
+					if(row.word.indexOf(' ') >= 0) {
+						game.the_word_data.has_whitespace = true;
+						game.the_word_data.whitespace_index = row.word.indexOf(' ');
+					} else {
+						game.the_word_data.has_whitespace = false;
+					}
 				}
 				game.start_intermission();	
 			});
@@ -238,10 +259,10 @@ io.on('connection', function(socket){
 	socket.on('msg', function (data) {
 		// check if the word to guess -- i guess
 		io.sockets.emit('update_chat', socket.username, data);
-		if(game.the_word === data && !game.is_drawer(socket.username) && game.player_is_drawing === true) {
+		if(game.the_word.toLowerCase() === data.toLowerCase() && !game.is_drawer(socket.username) && game.player_is_drawing === true) {
 			// guessed the word yo
 			io.sockets.emit('update_chat', 'SERVER', socket.username + ' was correct!');
-			game.guessed_right();
+			game.guessed_right(socket.username);
 		}
 	});
 	// when the client sends an image
@@ -263,12 +284,15 @@ io.on('connection', function(socket){
 		socket.username = username;
 		// add the client's username to the global list
 		usernames[username] = username;
+		// Scores too
+		scores[username] = 0;
 		// echo to client they've connected
-		socket.emit('update_chat', 'SERVER', 'you have connected');
+		socket.emit('update_chat', 'SERVER', 'Welcome ' + username + '! You have connected succesfully!');
 		// echo globally (all clients) that a person has connected
 		socket.broadcast.emit('update_chat', 'SERVER', username + ' has connected.');
+		console.log(usernames);
 		// update the list of users in chat, client-side
-		io.sockets.emit('update_users', usernames);
+		io.sockets.emit('update_users', usernames, scores);
 		socket.is_drawer = false;
 	});
 
@@ -296,7 +320,13 @@ io.on('connection', function(socket){
 					io.sockets.emit('intermission_counter_stop');
 					game.is_intermission = false;
 					delete players[socket.username];
-					rebuildqueue();					
+					rebuildqueue();	
+				} else if (game.is_cooldown === true) {
+					game.stop_timer();
+					io.sockets.emit('victory_counter_stop');
+					game.is_cooldown = false;
+					delete players[socket.username];
+					rebuildqueue();				
 				} else {
 					// The player is drawing
 					game.stop_timer();
@@ -309,15 +339,28 @@ io.on('connection', function(socket){
 				}
 				socket.is_drawer = false;
 				socket.emit('is_drawing', false);		
-				if (players.length <= 1) {
-					game_is_running = false;
+				if (Object.keys(players).length <= 1) {
+					game.is_running = false;
 					game.stop();
-				}		
+				} else {
+					game.stop();
+					game.run();
+				}
 			} else {
 				delete players[socket.username];
 				rebuildqueue();
-				if (players.length <= 1) {
-					game_is_running = false;
+				console.log(players.length);
+				if (Object.keys(players).length <= 1) {
+					game.is_running = false;
+					game.stop_timer();
+					if(game.is_intermission === true) {
+						io.sockets.emit('intermission_counter_stop');
+					} else if(game.is_cooldown === true) {
+						io.sockets.emit('victory_counter_stop');
+					} else {
+						io.sockets.emit('drawer_counter_stop');
+					}
+					game.the_word = "";
 					game.stop();
 				}
 			}
@@ -326,19 +369,74 @@ io.on('connection', function(socket){
 	});
 	
 	// when the user disconnects.. perform this
-	socket.on('disconnect', function(){
-		// remove the username from global usernames list
+	socket.on('disconnect', function() {
+		console.log('socket.disconnects');
 		delete usernames[socket.username];
-		// remove user from players
-		if (players[socket.username] !== undefined) {
+		delete scores[socket.username];
+		if (typeof players[socket.username] !== "undefined") {
 			delete players[socket.username];
 		}
+		rebuildqueue();
+		if(game.is_drawer(socket.username)) {
+			socket.is_drawer = false;
+			socket.emit('is_drawing', false);
+			if(game.is_intermission === true) {
+				game.stop_timer();
+				io.sockets.emit('intermission_counter_stop');
+				game.is_intermission = false;
+				game.stop();
+				if(Object.keys(players).length > 1) {
+					game.run();
+				}
+			} else if(game.is_cooldown === true) {
+                                game.stop_timer();
+				io.sockets.emit('victory_counter_stop');
+				game.is_cooldown = false;
+				game.stop();
+				if(Object.keys(players).length > 1) {
+					game.run();
+				}
+			} else {
+				game.stop_timer();
+				io.sockets.emit('drawer_counter_stop');
+				game.player_is_drawing = false;
+				game.the_word = "";
+				//delete players[socket.username];
+				game.stop();
+				if(Object.keys(players).length > 1) {
+					game.run();
+				}
+			}
+		} else {
+			if(Object.keys(players).length <= 1) {
+				game.stop();
+			}
+		}
+
+		// remove the username from global usernames list
+		/*delete usernames[socket.username];
+		delete scores[socket.username];
+		// remove user from players
+		if (typeof players[socket.username] !== "undefined") {
+			delete players[socket.username];
+		}*/
 		// update list of users in chat, client-side
-		io.sockets.emit('update_users', usernames);
+		io.sockets.emit('update_users', usernames, scores);
 		// echo globally that this client has left
 		socket.broadcast.emit('update_chat', 'SERVER', socket.username + ' has disconnected');
 	});
 	
+});
+
+io.on('disconnect', function(socket) {
+       	console.log('io.disconnects io.disconnect');	
+	delete usernames[socket.username];
+	delete scores[socket.username];
+	if (typeof players[socket.username] !== "undefined") {
+		delete players[socket.username];
+	}
+	io.sockets.emit('update_users', usernames, scores);
+	socket.broadcast.emit('update_chat', 'SERVER', socket.username + ' has disconnected');
 });
 
 // Fetches a socket with a players name
